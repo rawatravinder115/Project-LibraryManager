d4af6b2b7fc023616c1fea28855543391dca88cf
rawatravinder115 <rawatravinder115@gmail.com>
2021-05-29 20:02:13 +0530

    first commit

---
+++ Library-Manager/src/test/java/comProject/LibraryManager/LibraryManager/LibraryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package comProject.LibraryManager.LibraryManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
